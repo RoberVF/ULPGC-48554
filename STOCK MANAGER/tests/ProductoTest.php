673d686e8ceb397ce5c55395bb832f6d1b577d0e
Roberto Morales Fumero <robertomoralesfumero@gmail.com>
2025-10-04 12:47:07 +0100

    Test rojo, falta funcionalidad del modelo Producto

---
+++ STOCK MANAGER/tests/ProductoTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Models\Producto;
+
+class ProductoTest extends TestCase {
+    private $producto;
+
+    protected function setUp(): void {
+        $this->producto = new Producto();
+        $this->producto->crearTabla();
+        $this->producto->db->exec("DELETE FROM productos");
+    }
+
+    public function testAgregarYObtenerProducto() {
+        $id = $this->producto->agregarProducto("Agua", 19.99, 10);
+        $productos = $this->producto->obtenerProductos();
+
+        $this->assertNotEmpty($productos);
+        $this->assertEquals("Agua", $productos[0]['nombre']);
+    }
+}
